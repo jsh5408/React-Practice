ff7631db96d25ea69cbedd3015d6d08d1a198d79
Jun Seung Hyun <jsh5408@naver.com>
2021-12-06 23:48:14 +0900

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Home from "./Home";
 import CoinConverter from "./CoinConverter";
+import {Route, Link} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <CoinConverter />
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/coinconverter">CoinConverter</Link>
+        </li>
+      </ul>
+      <Route exact path="/home" component={Home} />
+      <Route path="/coinconverter" component={CoinConverter} />
     </div>
   );
 }
